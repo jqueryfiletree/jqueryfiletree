e116eab916bd8725cad2ac4089c048790cfcef58
Dave Rogers <redcore@gmail.com>
2015-02-27 00:48:31 -0800

    SECURITY: #10 - exposure of filesystem to directory mining (PHP)

--- connectors/jqueryFileTree.php
+++ connectors/jqueryFileTree.php
@@ -1,4 +1,6 @@
 <?php
+if( !array_key_exists('HTTP_REFERER', $_SERVER) ) exit('No direct script access allowed');
+
 /**
  * jQuery File Tree PHP Connector
  *
@@ -9,6 +11,7 @@
  *
  * History:
  *
+ * 1.1.1 - SECURITY: forcing root to prevent users from determining system's file structure (per DaveBrad)
  * 1.1.0 - adding multiSelect (checkbox) support (08/22/2014)
  * 1.0.2 - fixes undefined 'dir' error - by itsyash (06/09/2014)
  * 1.0.1 - updated to work with foreign characters in directory/file names (12 April 2008)
@@ -17,29 +20,43 @@
  * Output a list of files for jQuery File Tree
  */
 
-$_POST['dir'] = urldecode((isset($_POST['dir']) ? $_POST['dir'] : null ));
+/** 
+ * filesystem root - USER needs to set this!
+ * -> prevents debug users from exploring system's directory structure
+ * ex: $root = $_SERVER['DOCUMENT_ROOT']; 
+ */
+$root = null;
+if( !$root ) exit("ERROR: Root filesystem directory not set in jqueryFileTree.php");
+
+$postDir = urldecode($root.(isset($_POST['dir']) ? $_POST['dir'] : null ));
 
 // set checkbox if multiSelect set to true
 $checkbox = ( isset($_POST['multiSelect']) && $_POST['multiSelect'] == 'true' ) ? "<input type='checkbox' />" : null;
 
-if( file_exists($_POST['dir']) ) {
-	$files = scandir($_POST['dir']);
+if( file_exists($postDir) ) {
+
+	$files		= scandir($postDir);
+	$returnDir	= substr($postDir, strlen($root));
+
 	natcasesort($files);
+
 	if( count($files) > 2 ) { // The 2 accounts for . and ..
+
 		echo "<ul class='jqueryFileTree'>";
-		// All dirs
+
 		foreach( $files as $file ) {
-			if( file_exists($_POST['dir'] . $file) && $file != '.' && $file != '..' && is_dir($_POST['dir'] . $file) ) {
-				echo "<li class='directory collapsed'>{$checkbox}<a href='#' rel='" .htmlentities($_POST['dir'] . $file). "/'>" . htmlentities($file) . "</a></li>";
+			$htmlRel	= htmlentities($returnDir . $file);
+			$htmlName	= htmlentities($file);
+			$ext		= preg_replace('/^.*\./', '', $file);
+
+			if( file_exists($postDir . $file) && $file != '.' && $file != '..' ) {
+				if( is_dir($postDir . $file) )
+					echo "<li class='directory collapsed'>{$checkbox}<a href='#' rel='" .$htmlRel. "/'>" . $htmlName . "</a></li>";
+				else
+					echo "<li class='file ext_{$ext}'>{$checkbox}<a href='#' rel='" . $htmlRel . "'>" . $htmlName . "</a></li>";
 			}
 		}
-		// All files
-		foreach( $files as $file ) {
-			if( file_exists($_POST['dir'] . $file) && $file != '.' && $file != '..' && !is_dir($_POST['dir'] . $file) ) {
-				$ext = preg_replace('/^.*\./', '', $file);
-				echo "<li class='file ext_{$ext}'>{$checkbox}<a href='#' rel='" . htmlentities($_POST['dir'] . $file) . "'>" . htmlentities($file) . "</a></li>";
-			}
-		}
+
 		echo "</ul>";
 	}
 }
