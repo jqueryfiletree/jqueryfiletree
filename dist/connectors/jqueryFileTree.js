8461ddc7fb5ed63dc5d141f4054d63b339229afa
Vincent Liao <vliao@rmn.com>
2016-04-04 13:25:11 -0500

    Amend legacy Node.js connector to handle spaces in file path.

--- dist/connectors/jqueryFileTree.js
+++ dist/connectors/jqueryFileTree.js
@@ -11,23 +11,25 @@ var _getDirList = function(request, response) {
 	var r = '<ul class="jqueryFileTree" style="display: none;">';
    	try {
        	r = '<ul class="jqueryFileTree" style="display: none;">';
-		var files = fs.readdirSync(dir);
+		var dirDecoded = decodeURIComponent(dir);
+		var files = fs.readdirSync(dirDecoded);
 		files.forEach(function(f){
-			var ff = dir + f;
-			var stats = fs.statSync(ff)
+			var ff = dirDecoded + decodeURIComponent(f);
+			var ffEncoded = dir + encodeURIComponent(f);
+			var stats = fs.statSync(ff);
             if (stats.isDirectory()) { 
-                r += '<li class="directory collapsed"><a href="#" rel="' + ff  + '/">' + f + '</a></li>';
+                r += '<li class="directory collapsed"><a href="#" rel="' + ffEncoded + '/">' + f + '</a></li>';
             } else {
             	var e = f.split('.')[1];
-             	r += '<li class="file ext_' + e + '"><a href="#" rel='+ ff + '>' + f + '</a></li>';
+             	r += '<li class="file ext_' + e + '"><a href="#" rel='+ ffEncoded + '>' + f + '</a></li>';
             }
 		});
 		r += '</ul>';
 	} catch(e) {
-		r += 'Could not load directory: ' + dir;
+		r += 'Could not load directory: ' + dirDecoded;
 		r += '</ul>';
 	}
 	response.send(r)
-}
+};
 
 module.exports.getDirList = _getDirList;
